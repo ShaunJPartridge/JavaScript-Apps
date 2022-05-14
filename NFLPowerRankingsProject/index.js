a766186c64c383207590a3cba054343f536d7433
Shaun Partridge <spartridgeb15@gmail.com>
2022-05-14 12:06:27 -0400

    update

--- NFLPowerRankingsProject/index.js
+++
@@ -1,7 +0,0 @@
-Document doc = Jsoup.connect("https://en.wikipedia.org/").get();
-log(doc.title());
-Elements newsHeadlines = doc.select("#mp-itn b a");
-for (Element headline : newsHeadlines) {
-  log("%s\n\t%s", 
-    headline.attr("title"), headline.absUrl("href"));
-}
